1585d431b6748a9e00ca21b2064b832918896640
Simranwatts <simranwatts8@gmail.com>
2026-08-03 09:49:03 +0530

    Time: 0 ms (100%), Space: 8.8 MB (12.92%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-    return pow(x,n);
+        long binform = n;
+        double ans = 1;
+        if(n < 0){
+            x = 1/x;
+            binform = (-1)*binform;
+        }
+
+        while(binform > 0){
+            if(binform % 2 == 1){
+                ans *= x;
+            }
+            x *= x;
+            binform >>= 1;
+        }
+        return ans;
     }
 };

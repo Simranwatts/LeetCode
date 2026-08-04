08d9db9f2e05087630ecb3e2aacf9a5d8c4a2872
Simranwatts <simranwatts8@gmail.com>
2026-08-04 14:03:47 +0530

    Time: 3 ms (18.79%), Space: 71.8 MB (15.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,10 +5,10 @@ public:
         int maxSum = INT_MIN;
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
-            if(sum < nums[i]){
-                sum = nums[i];
-            }
             maxSum = max(maxSum, sum);
+            if(sum < 0){
+                sum = 0;
+            }
         }
         return maxSum;
     }

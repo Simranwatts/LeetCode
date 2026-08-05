d60e9595c19b645e86b03169e781d898098de797
Simran <simranwatts8@gmail.com>
2026-08-05 17:43:50 +0530

    renamed files

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array_sol2.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array_sol2.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n = arr.size();
-        int c;
         int s = 0, e = n-1;
         if(arr[s] > arr[s+1]) return 0;
         if(arr[e] > arr[e-1]) return e;
@@ -10,8 +9,7 @@ public:
         s++;
         while(s <= e){
             int mid = s + (e-s)/2;
-            if((arr[mid] > arr[mid-1]) & (arr[mid] > arr[mid+1])){
-                c = mid;
+            if((arr[mid] > arr[mid-1]) && (arr[mid] > arr[mid+1])){
                 return mid;
             }
             if(arr[mid] < arr[mid+1]){
@@ -21,6 +19,6 @@ public:
                 e= mid-1;
             }
         }
-        return c;
+        return -1;
     }
 };

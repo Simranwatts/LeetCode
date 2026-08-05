a31e836d8bc1ea91cea0d28e63bc5197a4e7d551
Simran <simranwatts8@gmail.com>
2026-08-05 17:41:15 +0530

    renamed files

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array_sol1.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array_sol1.cpp
@@ -7,7 +7,7 @@ public:
         if(arr[n-1] > arr[n-2]) return n-1;
 
         for(int i = 1; i < n-1; i++){
-            if((arr[i] > arr[i-1]) & (arr[i] > arr[i+1]))
+            if((arr[i] > arr[i-1]) && (arr[i] > arr[i+1]))
             c = i;
         }
         return c;

7b0c7bc0de365cfba989bbe429da9a963293e640
Simranwatts <simranwatts8@gmail.com>
2026-08-11 16:02:51 +0530

    Time: 0 ms (100%), Space: 26.2 MB (36.15%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,7 +11,7 @@ public:
         while(s < e){
             int mid = s + (e - s) / 2;
             if(mid % 2 == 0){
-                if(nums[mid] == nums[mid+1]) s = mid+1;
+                if(nums[mid] == nums[mid+1]) s = mid+2;
                 else e = mid;
             }
             else{

04c35227d556a1d7fcdae05203cf3f73b33f4588
Simranwatts <simranwatts8@gmail.com>
2026-08-19 14:42:49 +0530

    Time: 0 ms (100%), Space: 15.8 MB (14.52%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int piv = -1;
+        int n = nums.size();
+
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                piv = i;
+                break;
+            }
+        }
+        if(piv == -1){
+            int s = 0;
+            int e = n - 1;
+
+            while(s < e){
+                swap(nums[s], nums[e]);
+                s++;
+                e--;
+            }
+            return;
+        }
+
+        for(int i = n - 1; i > piv; i--){
+            if(nums[i] > nums[piv]){
+                swap(nums[i], nums[piv]);
+                break;
+            }
+        }
+        int s = piv + 1;
+        int e = n - 1;
+
+        while(s < e){
+            swap(nums[s], nums[e]);
+            s++;
+            e--;
+        }
+    }
+};
